0de67c47bd44fb263da1abdf40e65db513932de9
bersoncrios <bersonintech@gmail.com>
2023-10-28 14:53:02 -0300

    feat(logout): make logout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
@@ -11,12 +12,17 @@ export class HeaderComponent implements OnInit{
   token = sessionStorage.getItem('x-dilly-token');
 
 
-  constructor(private jwtHelper: JwtHelperService){}
+  constructor(private jwtHelper: JwtHelperService, private router: Router){}
 
   ngOnInit(): void {
   }
 
   isTokenValid(): boolean {
     return !this.jwtHelper.isTokenExpired(this.token);
+  }
+
+  logout(){
+    this.router.navigate(["/"])
+    sessionStorage.removeItem('x-dilly-token')
   }
 }
